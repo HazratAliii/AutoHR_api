ad5744725a4959f75128eceed10fec3589e46b3e
Hazrat Ali <hazratali3711@gmail.com>
2024-07-16 13:20:57 +0600

    feat: added users crud

---
+++ src/user/user.service.ts
@@ -0,0 +1,64 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    try {
+      const users = await this.prisma.user.findMany();
+      return users;
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+  }
+
+  async findOne(id: string) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (user) {
+        return user;
+      } else {
+        throw new NotFoundException();
+      }
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  async remove(id: string) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (user) {
+        return await this.prisma.user.delete({
+          where: {
+            id,
+          },
+        });
+      } else {
+        throw new NotFoundException();
+      }
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+  }
+}
